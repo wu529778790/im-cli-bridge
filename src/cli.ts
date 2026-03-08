ad4cc9fe9e25a9160e14e95da7644c1861cd6c52
神族九帝 <529778790@qq.com>
2026-03-08 23:55:58 +0800

    chore: 添加 stop 命令

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
 import { main } from './index.js';
+import { exec } from 'node:child_process';
 
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+const args = process.argv.slice(2);
+
+if (args[0] === 'stop') {
+  exec("pkill -f 'node dist/index.js'", (err) => {
+    if (err) {
+      console.log('未找到运行中的服务');
+    } else {
+      console.log('服务已停止');
+    }
+  });
+} else {
+  main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
